860ca62154eb72f2582363156bfc3b4b9f02509b
khencel <khenneth.alaiza@gmail.com>
2026-07-16 18:48:44 +0800

    update

--- src/data/gallery.tsx
+++ src/data/gallery.tsx
@@ -52,10 +52,100 @@ export function getGallery(){
 
         // interview
         { id: 40, category: "interview", imageUrl:"/img/gallery/interview/1.jpg" },
-        { id: 41, category: "interview", imageUrl:"/img/gallery/interview/37.jpeg" },
-        { id: 42, category: "interview", imageUrl:"/img/gallery/interview/38.jpeg" },
-        { id: 43, category: "interview", imageUrl:"/img/gallery/interview/39.jpeg" },
-        { id: 44, category: "interview", imageUrl:"/img/gallery/interview/39.jpeg" },
-        { id: 45, category: "interview", imageUrl:"/img/gallery/interview/39.jpeg" },
+        { id: 41, category: "interview", imageUrl:"/img/gallery/interview/2.jpg" },
+        { id: 42, category: "interview", imageUrl:"/img/gallery/interview/3.jpg" },
+        { id: 43, category: "interview", imageUrl:"/img/gallery/interview/4.jpg" },
+        { id: 44, category: "interview", imageUrl:"/img/gallery/interview/5.jpg" },
+        { id: 45, category: "interview", imageUrl:"/img/gallery/interview/6.jpg" },
+
+        { id: 46, category: "interview", imageUrl:"/img/gallery/interview/7.jpg" },
+        { id: 47, category: "interview", imageUrl:"/img/gallery/interview/8.jpg" },
+        { id: 48, category: "interview", imageUrl:"/img/gallery/interview/9.jpg" },
+        { id: 49, category: "interview", imageUrl:"/img/gallery/interview/10.jpg" },
+        { id: 50, category: "interview", imageUrl:"/img/gallery/interview/11.jpg" },
+        { id: 51, category: "interview", imageUrl:"/img/gallery/interview/12.jpg" },
+
+        
+        { id: 52, category: "interview", imageUrl:"/img/gallery/interview/14.jpg" },
+        { id: 53, category: "interview", imageUrl:"/img/gallery/interview/15.jpg" },
+        { id: 54, category: "interview", imageUrl:"/img/gallery/interview/16.jpg" },
+        { id: 55, category: "interview", imageUrl:"/img/gallery/interview/17.jpg" },
+        { id: 56, category: "interview", imageUrl:"/img/gallery/interview/18.jpg" },
+
+
+
+        { id: 57, category: "interview", imageUrl:"/img/gallery/interview/19.jpg" },
+        { id: 58, category: "interview", imageUrl:"/img/gallery/interview/20.jpg" },
+        { id: 59, category: "interview", imageUrl:"/img/gallery/interview/21.jpeg" },
+        { id: 60, category: "interview", imageUrl:"/img/gallery/interview/22.jpeg" },
+        { id: 61, category: "interview", imageUrl:"/img/gallery/interview/23.jpeg" },
+        { id: 62, category: "interview", imageUrl:"/img/gallery/interview/24.jpeg" },
+        { id: 63, category: "interview", imageUrl:"/img/gallery/interview/25.jpeg" },
+        { id: 64, category: "interview", imageUrl:"/img/gallery/interview/26.jpeg" },
+        { id: 65, category: "interview", imageUrl:"/img/gallery/interview/27.jpeg" },
+        { id: 66, category: "interview", imageUrl:"/img/gallery/interview/28.jpeg" },
+        { id: 67, category: "interview", imageUrl:"/img/gallery/interview/29.jpeg" },
+        { id: 68, category: "interview", imageUrl:"/img/gallery/interview/30.jpeg" },
+        { id: 69, category: "interview", imageUrl:"/img/gallery/interview/31.jpeg" },
+        { id: 70, category: "interview", imageUrl:"/img/gallery/interview/32.jpeg" },
+        { id: 71, category: "interview", imageUrl:"/img/gallery/interview/33.jpeg" },
+        { id: 72, category: "interview", imageUrl:"/img/gallery/interview/34.jpeg" },
+        { id: 73, category: "interview", imageUrl:"/img/gallery/interview/35.jpeg" },
+        { id: 74, category: "interview", imageUrl:"/img/gallery/interview/36.jpeg" },
+        { id: 75, category: "interview", imageUrl:"/img/gallery/interview/13.jpg" },
+
+        { id: 76, category: "interview", imageUrl:"/img/gallery/interview/37.jpeg" },
+        { id: 77, category: "interview", imageUrl:"/img/gallery/interview/38.jpeg" },
+        { id: 78, category: "interview", imageUrl:"/img/gallery/interview/39.jpeg" },
+        { id: 79, category: "interview", imageUrl:"/img/gallery/interview/40.jpeg" },
+        { id: 80, category: "interview", imageUrl:"/img/gallery/interview/41.jpeg" },
+        { id: 81, category: "interview", imageUrl:"/img/gallery/interview/42.jpeg" },
+
+        { id: 82, category: "interview", imageUrl:"/img/gallery/interview/43.jpeg" },
+        { id: 83, category: "interview", imageUrl:"/img/gallery/interview/44.jpeg" },
+        { id: 84, category: "interview", imageUrl:"/img/gallery/interview/45.jpeg" },
+        { id: 85, category: "interview", imageUrl:"/img/gallery/interview/46.jpeg" },
+        { id: 86, category: "interview", imageUrl:"/img/gallery/interview/47.jpeg" },
+        
+
+        // Skill
+        { id: 87, category: "tradetest", imageUrl:"/img/gallery/tradeTests/1.jpg" },
+        { id: 88, category: "tradetest", imageUrl:"/img/gallery/tradeTests/5.jpeg" },
+        { id: 90, category: "tradetest", imageUrl:"/img/gallery/tradeTests/9.jpeg" },
+
+        { id: 91, category: "facilities", title:"Angono" , imageUrl:"/img/gallery/facilities/ango1.jpeg" },
+        { id: 92, category: "facilities", title:"Angono" , imageUrl:"/img/gallery/facilities/ango2.jpeg" },
+        { id: 93, category: "facilities", title:"Angono" , imageUrl:"/img/gallery/facilities/ango3.jpeg" },
+        { id: 94, category: "facilities", title:"Angono" , imageUrl:"/img/gallery/facilities/ango4.jpeg" },
+        { id: 95, category: "facilities", title:"Angono" , imageUrl:"/img/gallery/facilities/ango5.jpeg" },
+        { id: 96, category: "facilities", title:"Bacolod" , imageUrl:"/img/gallery/facilities/baco1.jpeg" },
+        { id: 97, category: "facilities", title:"Bacolod" , imageUrl:"/img/gallery/facilities/baco2.jpeg" },
+        { id: 98, category: "facilities", title:"Bacolod" , imageUrl:"/img/gallery/facilities/baco3.jpeg" },
+        { id: 99, category: "facilities", title:"Bacolod" , imageUrl:"/img/gallery/facilities/baco4.jpeg" },
+        { id: 100, category: "facilities", title:"Bacolod" , imageUrl:"/img/gallery/facilities/baco5.jpeg" },
+
+        { id: 101, category: "facilities", title:"Bicol" , imageUrl:"/img/gallery/facilities/bicol1.jpeg" },
+        { id: 102, category: "facilities", title:"Bicol" , imageUrl:"/img/gallery/facilities/bicol2.jpeg" },
+        { id: 103, category: "facilities", title:"Bicol" , imageUrl:"/img/gallery/facilities/bicol3.jpeg" },
+        { id: 104, category: "facilities", title:"Bicol" , imageUrl:"/img/gallery/facilities/bicol4.jpeg" },
+        { id: 105, category: "facilities", title:"Bicol" , imageUrl:"/img/gallery/facilities/bicol5.jpeg" },
+
+        { id: 106, category: "facilities", title:"Butuan" , imageUrl:"/img/gallery/facilities/butuan1.jpeg" },
+        { id: 107, category: "facilities", title:"Butuan" , imageUrl:"/img/gallery/facilities/butuan2.jpeg" },
+        { id: 108, category: "facilities", title:"Butuan" , imageUrl:"/img/gallery/facilities/butuan3.jpeg" },
+        { id: 109, category: "facilities", title:"Butuan" , imageUrl:"/img/gallery/facilities/butuan4.jpeg" },
+        { id: 110, category: "facilities", title:"Butuan" , imageUrl:"/img/gallery/facilities/butuan5.jpeg" },
+        { id: 111, category: "facilities", title:"Butuan" , imageUrl:"/img/gallery/facilities/butuan6.jpeg" },
+        { id: 112, category: "facilities", title:"Butuan" , imageUrl:"/img/gallery/facilities/butuan7.jpeg" },
+
+        { id: 113, category: "facilities", title:"Cagayan De Oro" , imageUrl:"/img/gallery/facilities/cdo1.jpeg" },
+        { id: 114, category: "facilities", title:"Cagayan De Oro" , imageUrl:"/img/gallery/facilities/cdo2.jpeg" },
+        { id: 115, category: "facilities", title:"Cagayan De Oro" , imageUrl:"/img/gallery/facilities/cdo3.jpeg" },
+        { id: 116, category: "facilities", title:"Cagayan De Oro" , imageUrl:"/img/gallery/facilities/cdo4.jpeg" },
+
+        { id: 117, category: "facilities", title:"Cebu" , imageUrl:"/img/gallery/facilities/cebu1.jpeg" },
+        { id: 118, category: "facilities", title:"Cebu" , imageUrl:"/img/gallery/facilities/cebu2.jpeg" },
+        { id: 119, category: "facilities", title:"Cebu" , imageUrl:"/img/gallery/facilities/cebu3.jpeg" },
+        { id: 120, category: "facilities", title:"Cebu" , imageUrl:"/img/gallery/facilities/cebu4.jpeg" },
     ]
 }
